fec27d0e97f6d3fae6e0114aa3a8d9b355ff5cf6
Kiize <paolozzobibi@gmail.com>
2024-05-08 14:40:13 +0000

    add function

--- test.f90
+++ test.f90
@@ -1,14 +1,31 @@
+Real*8 Function c(r)
+    IMPLICIT NONE
+    Real*8 :: r
+    Real*8 :: PI = 3.14159265358979323846
+
+    c = 2.0 * r * PI
+end
+
+Real*8 Function a(r)
+    IMPLICIT NONE
+    Real*8 :: r
+    Real*8 :: PI = 3.14159265358979323846
+
+    a = r ** 2 * PI
+end
+
 PROGRAM AREA_CIRC
     IMPLICIT NONE
-    Real*8 :: radius, circ, area
+    Real*8 :: radius, circ, area, c, a
     Real*8 :: PI = 3.14159265358979323846
 
     print *, "Enter the radius: "
     read *, radius
 
-    circ = 2.0 * PI * radius
-    area = radius * radius * PI
+    circ = c(radius)
+    area = a(radius)
 
+    print *, "The radius is ", radius
     print *, "The circumference is ", circ
     print *, "While the area is", area
 
