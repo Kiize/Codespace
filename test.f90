72dda253b52988adbaac30093dd729d2bfe41af6
Kiize <paolozzobibi@gmail.com>
2024-05-08 14:11:52 +0000

    update test.f90

--- test.f90
+++ test.f90
@@ -1,4 +1,15 @@
-Program 
-    
+PROGRAM AREA_CIRC
+    IMPLICIT NONE
+    Real*8 :: radius, circ, area
+    Real*8 :: PI = 3.14159265358979323846
 
-End Program
+    print *, "Enter the radius: "
+    read *, radius
+
+    circ = 2.0 * PI * radius
+    area = radius * radius * PI
+
+    print *, "The circumference is ", circ
+    print *, "While the area is", area
+
+END PROGRAM AREA_CIRC
